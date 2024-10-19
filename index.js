4e8506e52335cd143b8ca02e33b8b05df7614ff0
WaelMohdAlshafey <smael7108@gmail.com>
2024-10-19 15:01:31 +0300

    first1

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('headings').style.color = 'red';
+document.getElementById("headings").style.margin = '10em';
+document.getElementById('headings').style.background ='blue'
+document.getElementById('headings').style.margin = '15em'
